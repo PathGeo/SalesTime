d67ba53a3be7975839893bd544a3bfaf8573b43d
jsaltenberger <j.saltenberger@gmail.com>
2013-03-06 11:03:07 -0800

    Add reviews.js for reputation/visibilty widgets

---
+++ demo/js/reviews.js
@@ -0,0 +1,42 @@
+var reviews = [
+	{
+		"Date": "2/22/2013",
+		"Review": "Worst car buying experience I have ever had. ÿMy family was long-time buyers from the company and felt it would be the best. ÿWhat a mistake.",
+		"Rating": "-5"
+	},
+	{
+		"Date": "2/18/2103",
+		"Review": "Drew Ford is super popular for a reason. You see their license plates tags everywhere! I even saw one out in LA last week. Imagine that!",
+		"Rating": "+3"
+	},
+	{
+		"Date": "2/4/2013",
+		"Review": "Great, friendly people. Always treats our family. ÿService and sales are top notch.",
+		"Rating": "+2"
+	},
+	{
+		"Date": "1/28/2013",
+		"Review": "I just bought a new vehicle through Drew Ford in La Mesa. ÿThe staff on the sales floor, the collision center and the service department were all professional and polite.",
+		"Rating": "+5"
+	},
+	{
+		"Date": "1/2/2013",
+		"Review": "Always treats our family. Service and sales are top notch.",
+		"Rating": "+3"
+	},
+	{
+		"Date": "12/12/2012",
+		"Review": "It's possible I just had a bad car salesman, but this experience at Drew Ford kind of made me lose all faith in their service.",
+		"Rating": "-3"
+	},
+	{
+		"Date": "11/18/2012",
+		"Review": "Just purchased my 2012 Ford Focus with Drew Ford. The sales staff was very helpful and were not the pushy sales people I had expected when I first arrived.",
+		"Rating": "+5"
+	},
+	{
+		"Date": "11/2/2012",
+		"Review": "I was upset with one employee. The owner was nice and professional.",
+		"Rating": "-1"
+	}
+]
