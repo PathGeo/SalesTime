71212bd525167ab00857817c71dcd466496b7059
Su Han <suhanmappingideas@gmail.com>
2013-03-07 01:33:48 -0800

    su
    I wanted to put this in the layout.  But I haven't figure out how I can
    combine my table with the current table in the layout. When I click each
    row in the current table, it blocks the entire screen so the map is not
    shown.  @_@
    
    I am still debugging my code.  Bug 1: the arrow does not indicate the
    correct location when the table is sorted. Bug2: Red colored text in the
    popup is not shown in the I.E.

---
+++ demo/temp/main.js
@@ -0,0 +1,433 @@
+var geoJsonData = 
+		
+{
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "properties": { "date": "2013\/02\/26", "text": "Cant drive my new car till i get shocks. Fack", "score": 83, "user": "Mantis619", "loc": "San Diego" }, "geometry": { "type": "Point", "coordinates": [ -117.100530, 32.749098 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/22", "text": "Car is over hearing weh  need to fix tomorrow", "score": 87, "user": "marileeaze", "loc": "SanDiego" }, "geometry": { "type": "Point", "coordinates": [ -117.158222, 32.778265 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/23", "text": "Who knows about intakes? I want to buy one for my car but I don't know which one?", "score": 79, "user": "2manyjuans", "loc": "San Diego" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/25", "text": "Then ima get my new tires+rims. Finalllllyyyy ! Babygirl gonna be more rideable lol", "score": 84, "user": "gigglesbabyxoxo", "loc": "San Diego" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/26", "text": "My car needs a damn tune up &amp; oil change", "score": 75, "user": "nsolis23", "loc": "Chula Vista" }, "geometry": { "type": "Point", "coordinates": [ -117.001780, 32.633332 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/24", "text": "Lmao my car just died on me again. I need new a new battery for my car", "score": 79, "user": "ChelseyChavez", "loc": "San Diego, California" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/25", "text": "All looking up getting my car painted next week", "score": 80, "user": "2Girlsx1STONER_", "loc": "San Diego" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/25", "text": "OF COURSE my car needs a new battery....so much for good luck #frustrated", "score": 72, "user": "negsmani", "loc": "San Diego" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/26", "text": "grrr...I hate my car ! Expensive repairs and still something new to break.", "score": 76, "user": "MagdaBanome", "loc": "Chula Vista" }, "geometry": { "type": "Point", "coordinates": [ -117.083330, 32.640000 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/23", "text": "- ok . We're just waiting on tia to bring the money to fix gmas car -_______- @_Jacintaaa", "score": 81, "user": "Kitana_Monaee", "loc": "San Diego" }, "geometry": { "type": "Point", "coordinates": [ -117.100530, 32.749098 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/18", "text": "Car shopping today", "score": 90, "user": "sarahmikhael_", "loc": "San Diego" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/19", "text": "I think I found someone to buy my car. Now looking for a new car.", "score": 93, "user": "shhhua", "loc": "San Diego, CA" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/21", "text": "Looking for a new car to buy", "score": 71, "user": "Ryro1313", "loc": "Vista, CA" }, "geometry": { "type": "Point", "coordinates": [ -117.241700, 33.200000 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/25", "text": "Looks like im getting a new car. Welp.", "score": 88, "user": "mariesammy", "loc": "San Diego" }, "geometry": { "type": "Point", "coordinates": [ -117.158222, 32.778265 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/16", "text": "Car shopping. Too many cars to choose from", "score": 89, "user": "cabrera_gabe", "loc": "San Diego, CA" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/26", "text": "Can't wait to get my new car in the summer.  Trying to race @Freddyx24x", "score": 65, "user": "LuisxNights", "loc": "Chula Vista" }, "geometry": { "type": "Point", "coordinates": [ -117.083330, 32.640000 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/22", "text": "Gettin a new car=)", "score": 86, "user": "Kenzie_Kmackin", "loc": "Mira Mesa" }, "geometry": { "type": "Point", "coordinates": [ -117.152191, 32.905016 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/20", "text": "I guess it's time for a new car. Shopped around a little today... I'm getting excited", "score": 96, "user": "Cfendiz", "loc": "San Diego, CA" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/22", "text": "Time for a new car!!!!!", "score": 85, "user": "STEEZEandCHEEZE", "loc": "San Diego" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+,
+{ "type": "Feature", "properties": { "date": "2013\/02\/21", "text": "This new car situation looking good :)", "score": 83, "user": "Da_Juan_andOnly", "loc": "San Diego" }, "geometry": { "type": "Point", "coordinates": [ -117.156400, 32.715300 ] } }
+
+]
+}
+
+
+	// start map functions
+	var hitMapData={
+    	max:  1,        // Always 1 in tweet data
+		data: []
+    };	
+		
+		
+	//var map = new L.Map('map');
+	var OpenStreet = 'http://{s}.tile.cloudmade.com/ad132e106cd246ec961bbdfbe0228fe8/997/256/{z}/{x}/{y}.png',
+	//apple = new L.TileLayer(OpenStreet, {maxZoom: 18,unloadInvisibleTiles: true});
+	    cmAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
+		cmUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/{styleId}/256/{z}/{x}/{y}.png';
+
+
+
+    var openstreet = L.tileLayer(OpenStreet, {styleId: 256, attribution: cmAttr})
+	    minimal    = L.tileLayer(cmUrl, {styleId: 22677, attribution: cmAttr}),
+	    midnight   = L.tileLayer(cmUrl, {styleId: 999,   attribution: cmAttr});
+	    //motorways = L.tileLayer(cmUrl, {styleId: 46561, attribution: cmAttr});
+		
+	var map = L.map('map', {
+		center: [32.834917, -117.005639],
+		//center: [0, 0],
+		zoom: 9,
+		layers: [minimal],
+		attributionControl:false
+	});			
+			
+	var heatmapLayer = L.TileLayer.heatMap({
+	    unloadInvisibleTiles: true,
+		reuseTiles: true,
+		radius:30,
+		opacity: 0.8,
+		gradient: {
+			0.45: "rgb(0,0,255)",
+			0.65: "rgb(0,255,255)",
+			0.85: "rgb(0,255,0)",
+			0.98: "yellow",
+			1.0: "rgb(255,0,0)"
+		}
+	});	
+
+	//map.setView(new L.LatLng(40.00, -100.387), 4).addLayer(apple);
+			
+	//var controls = L.layerGroup();
+
+	var baseLayers = {
+	    "Street":openstreet,
+		"Minimal": minimal,
+		//"Night View": midnight				
+	};
+
+       var controls = L.layerGroup().addTo(map);
+	
+
+        // cluster map
+		var obj = {name: "searchResult", 
+				type: "GEOJSON", 
+				json: geoJsonData,
+				srs: "EPSG:4326",
+				title: "Demo Data",
+				fieldName:{score:"score", username:"user", text:"text"},
+				keywords:["new car", "new battery", "my car"]};
+		var markers = pathgeo.layer.markerCluster
+			(
+				obj.json,
+				{onEachFeature: function(feature,layer){
+					var html="<div class='popup'><ul><li><img src='images/1359925009_twitter_02.png' width=20px />&nbsp; &nbsp; (<b>"+ feature.properties[obj.fieldName.score]+"</b>) "+ feature.properties[obj.fieldName.text]+" &nbsp;-&nbsp; "+ feature.properties[obj.fieldName.username]+"</li></ul></div>";
+					html=html.replace(/undefined/g, "Tweet");
+												
+					//highlight keyword
+					html=pathgeo.util.highlightKeyword(obj.keywords,html);
+					//info window
+					layer.bindPopup(html,{maxWidth:500, maxHeight:300});
+					}
+				},
+				{clusterclick: function(e){
+					if(!e.layer._popup){
+						var properties=pathgeo.util.readClusterFeatureProperies(e.layer, []);
+						var html="<div class='popup'>There are <b>" + e.layer._childCount + "</b> twitters:<p></p><ul>";
+						$.each(properties, function(i, property){
+							html+="<li><img src='images/1359925009_twitter_02.png' width=20px />&nbsp; &nbsp; (<b>"+ property[obj.fieldName.score]+"</b>) "+ property[obj.fieldName.text]+" &nbsp;-&nbsp; "+ property[obj.fieldName.username]+"</li>";
+							});
+						html+="</ul></div>";
+						html=html.replace(/undefined/g, "Tweet");
+										
+						//highlight keyword
+						html=pathgeo.util.highlightKeyword(obj.keywords,html);
+												
+						e.layer.bindPopup(html,{maxWidth:500, maxHeight:300}).openPopup();
+					}else{
+						e.layer.openPopup();
+						}
+					}
+				}
+			);
+//			app.controls.toc.addOverlay(obj.markerClusterLayer, "MarkerCluster");
+		//markers.addLayer(geoJsonLayer);
+
+		map.addLayer(markers);
+		map.fitBounds(markers.getBounds());	
+		
+/*		
+		var markers = new L.MarkerClusterGroup();
+
+		var geoJsonLayer = L.geoJson(geoJsonData, {
+			onEachFeature: function (feature, layer) {
+				layer.bindPopup(feature.properties.text);
+			}
+		});
+		markers.addLayer(geoJsonLayer);
+
+		map.addLayer(markers);
+		map.fitBounds(markers.getBounds());	
+*/
+	//var bingGeocoder = new L.Control.BingGeocoder('AvZ8vsxrtgnSqfEJF1hU40bASGwxahJJ3_X3dtkd8BSNljatfzfJUvhjo9IGP_P7');
+
+	//map.addControl(bingGeocoder);
+
+		// add a marker in the given location, attach some popup content to it and open the popup
+	
+		var pointIcon = L.icon({
+		iconUrl: 'ford.png',
+		iconSize:     [60, 35], // size of the icon
+		});
+		
+		var red = L.icon({
+		iconUrl: 'arrow.png',
+		iconAnchor:  [18,-1], // point from which the popup should open relative to the iconAnchor
+		iconSize:     [25, 35], // size of the icon
+		});		
+		
+	var overlays = {
+		//"tweets": controls,
+		//"heatmap": heatmapLayer,
+		"Cluster": markers
+	};
+	L.control.layers(baseLayers, overlays).addTo(map);	
+		
+/*		
+		// add markers for service
+		var markerService = [];
+		for (var i=0; i<leads.service.length; i++){
+			var service = leads.service[i];
+			markerService[i] = new L.CircleMarker(new L.LatLng(service.latlon[0], service.latlon[1]), {color: 'red'}).addTo(map);
+		}
+		
+		// add markers for sales
+		var markerSales = [];
+		for (var i=0; i<leads.sales.length; i++){
+			var sales = leads.sales[i];
+			markerSales[i] = new L.CircleMarker(new L.LatLng(sales.latlon[0], sales.latlon[1]), {color: 'red'}).addTo(map);
+		}
+		
+*/
+	
+		var markerDealer = L.marker([32.774917,-117.005639], {icon: pointIcon}).addTo(map);
+		//markerDealer.bindPopup("Drew Ford Dealership").openPopup();
+				
+
+/*		
+		var marker2 = L.CircleMarker(new L.LatLng(32.769324,-117.003322).addTo(map);
+		marker2.bindPopup("Test driving a Ford Fusion today");
+		
+		var marker3 = L.marker([32.7712,-117.010639]).addTo(map);
+		marker3.bindPopup("Just crashed my car :(... need a new one now");
+		
+		var marker4 = L.marker([32.775115,-117.000275]).addTo(map);
+		marker4.bindPopup("Can't wait to see the new 2013 Mustang!");
+		
+		var marker5 = L.marker([32.759779,-117.136316]).addTo(map);
+		marker5.bindPopup("Shelby GT is too much, guess I'm getting a Focus");
+*/
+
+
+
+	
+	// end - map functions
+	
+	// start - table
+		
+    google.load("visualization", "1", {packages:["corechart"]});
+    google.load('visualization', '1', {packages:['table']});
+    google.load('visualization', '1', {packages:['gauge']});
+    google.setOnLoadCallback(drawChart);
+	
+    function drawChart() {
+	
+		var SelectedMarker = null;
+  		
+        // Draw Google Table [Service Leads] - start
+        var dataService = new google.visualization.DataTable();
+		
+		// Set colmns for Service Leads
+		dataService.addColumn('number', 'Score');
+        dataService.addColumn('string', 'Tweet');
+		dataService.addColumn('string', 'Date');
+		dataService.addColumn('string', 'User');
+		dataService.addColumn('string', 'Contact');
+
+		// Set each row for Service Leads
+		for (var i=0; i<leads.service.length; i++){
+			var service = leads.service[i];
+			//text=pathgeo.util.highlightKeyword(obj.keywords,service.text);
+			//alert(text);
+			dataService.addRow([service.score, service.text, service.date, service.user, "know more"]);
+		}
+		dataService.sort({column:0, desc:true});
+		
+		// Show table of Service Leads
+        var tableService = new google.visualization.Table(document.getElementById('table_service'));
+        tableService.draw(dataService, {showRowNumber: false});
+	    
+	    // Add our selection handler
+	    google.visualization.events.addListener(tableService, 'select', selectService);
+	    	    
+	    // Select a table row for Service Leads
+	    function selectService() {
+	    	var selection = tableService.getSelection();
+	    	var row = selection[0].row;
+			var service = leads.service[row];
+			drawMarker(service.latlon[0], service.latlon[1]);
+	    }
+		// Draw Google Table [Service Leads] - end
+
+
+        // Draw Google Table [Sales Leads] - start
+        var dataSales = new google.visualization.DataTable();
+		
+		// Set colmns for Sales Leads
+		dataSales.addColumn('number', 'Score');
+        dataSales.addColumn('string', 'Tweet');      
+        //dataSales.addColumn('string', 'Location');
+		dataSales.addColumn('string', 'Date');
+		dataSales.addColumn('string', 'User');
+		dataSales.addColumn('string', 'Contact');
+		
+		// Set each row for Sales Leads
+		for (var i=0; i<leads.sales.length; i++){
+			var sales = leads.sales[i];
+			dataSales.addRow([sales.score, sales.text, sales.date, sales.user, "know more"]);
+		}
+		dataSales.sort({column:0, desc:true});
+
+		// Show table of Sales Leads
+        var tableSales = new google.visualization.Table(document.getElementById('table_sales'));
+        tableSales.draw(dataSales, {showRowNumber: false});
+	    
+	    // Add our selection handler
+	    google.visualization.events.addListener(tableSales, 'select', selectSales);
+	    	    
+	    // Select a table row for Sales Leads
+	    function selectSales() {
+	    	var selection = tableSales.getSelection();
+	    	var row = selection[0].row;
+			var sales = leads.sales[row];
+			drawMarker(sales.latlon[0], sales.latlon[1]);
+	    }
+		// Draw Google Table [Sales Leads] - end
+
+		
+		// Draw marker on the map
+		function drawMarker(lat, lon) {			
+			if (SelectedMarker != null) map.removeLayer(SelectedMarker);        // Remove marker if set
+			//alert(lat + " " + lon);
+			//SelectedMarker = new L.Marker(new L.LatLng(lat, lon));
+		    //map.addLayer(SelectedMarker);
+			//SelectedMarker = new L.CircleMarker(new L.LatLng(lat, lon), {color: 'red'}).addTo(map);
+			//var SelectedMarker = L.marker([lat,lon], {icon: red}).addTo(map);
+			SelectedMarker = L.marker([lat,lon], {icon: red}).addTo(map);
+		}
+	    
+
+    }
+    // end - table		
+	
+
+		
+	function getTweets() {
+	
+	
+	
+		var params = {};  //build an object, then pass it!
+		var param = $("#keyword").val() || 'NULL';
+		var rad = $("#radius").val() || 0;
+		var keywd = $("#car").val()
+           	
+		controls.clearLayers();
+		hitMapData.data = [];        // Clear all items of hit map
+	
+		var url = "PyMapper.py?key=" + param + "&rad=" + rad + "&keyword=" + keywd;
+
+		$.getJSON(url, function(data) {
+			var tweets = data.results;
+			
+			if (!tweets.length) {
+				$("#table tbody tr").remove();	
+				$("#table").css("display", (tweets.length > 0) ? "block" : "none");
+				$("#urls").css("display", (data.urls.length > 0) ? "block" : "none");	
+				$("#urls tbody tr").remove();		
+				//$("#results").html("[" + tweets.length + " results.]");
+				return;
+			}
+			
+			$("#table tbody tr").remove();	
+			$("#table").css("display", (tweets.length > 0) ? "block" : "none");
+			
+			var minLat = 180.0,
+				maxLat = -180.0,
+				minLon = 180.0,
+				maxLon = -180.0;
+				
+			//alert(hitMapData.data.toSource());
+			//hitMapData ='';
+			for(var indx in tweets)  {
+				var tweet = tweets[indx];
+                  
+				if (!(tweet.loc[0] == 0 && tweet.loc[1] == 0)) {
+					//controls.removeLayer(heatmapLayer);
+					//keep track of min/max lat/lon to set view
+					if (tweet.loc[0] > maxLon) maxLon = tweet.loc[0];
+					if (tweet.loc[0] < minLon) minLon = tweet.loc[0];
+					if (tweet.loc[1] > maxLat) maxLat = tweet.loc[1];
+					if (tweet.loc[1] < minLat) minLat = tweet.loc[1];
+				   
+					controls.addLayer(L.marker(tweet.loc).bindPopup(tweet.text));
+					var heatItem = {lat:tweet.loc[0], lon:tweet.loc[1], value:1};
+					hitMapData.data.push(heatItem);
+					$("#table tbody").append("<tr><td><div style='height: 50px;'>" + tweet.text + "</div></td></tr>");
+				
+				}
+				
+			}
+			//alert(hitMapData.data.toSource());
+			$("#table tr:even").css("background-color", "#ccc");
+			//$("#results").html("[" + tweets.length + " results.]");
+			
+			map.fitBounds([[minLon, minLat], [maxLon, maxLat]]);
+            L.Util.requestAnimFrame(map.invalidateSize,map,!1,map._container);
+               map.invalidateSize(); 
+
+			
+			var urls = data.urls;
+			$("#urls").css("display", (urls.length > 0) ? "block" : "none");	
+			$("#urls tbody tr").remove();		
+			for (var i = 0; i < urls.length; i++) {
+				$("#urls tbody").append("<tr><td><a href='" + urls[i][0] + "' target='_blank'>" + urls[i][0] + "</a></td><td style='padding-left: 40px;'>" + urls[i][1] + "</td></tr>");
+			}
+			$("#urls table tr:even").css("background-color", "#ccc");
+
+			 
+		    heatmapLayer.addData(hitMapData.data); 
+			
+		    controls.addLayer(heatmapLayer);
+/*
+			var overlays = {
+				"tweets": controls,
+				"heatmap": heatmapLayer
+			};
+			
+			L.control.layers(baseLayers, overlays).addTo(map);
+			
+			
+*/
+
+
+						
+		});
+      			
+	}
+
+	
+	$(document).ready(function() { 	    
+		// north-center
+	    //getTweets();
+        L.Util.requestAnimFrame(map.invalidateSize,map,!1,map._container);
+        map.invalidateSize();		
+		
+
+   	
+	});
+	
+	
+		
