048e4ad6b95642734711cfdd7764c748b5860af0
Chris Allen <cccchris.allen@gmail.com>
2013-03-19 22:15:35 -0700

    Clean up old code.

--- demo/js/main.js
+++ demo/js/main.js
@@ -70,30 +70,9 @@
 				showDialog("dialog_news", "news", {modal:true, resizable:false, draggable:false, width:900, height:650, close:function(e,ui){$("#dialog_news iframe").html("").attr("src", "");}});
 			}
 		})
-		
-		//use google chart table to show the rssFeeds
-//		var rssData = new google.visualization.DataTable();
-//		rssData.addColumn("number", "Score");
-//		rssData.addColumn("string", "Date");
-//		rssData.addColumn("string", "Source");
-//		rssData.addColumn("string", "Title");
-//		
-//		for (var indx in rssFeeds) {
-//			var feed = rssFeeds[indx];
-//			rssData.addRow( [ 	feed.score, 
-//								feed.date, feed.name, 
-//								"<a style='color: #22A' title= 'Click to see article.' target='_blank' href='" 
-//										+ feed.url + "'>" + pathgeo.util.highlightKeyword(app.constants.KEYWORDS, feed.title, true) + "</a>" 
-//							] );
-//		}
-//		
-//		var rssTable = new google.visualization.Table(document.getElementById('rss_news'));
-//		rssTable.draw(rssData, { showRowNumber: false, allowHtml: true, sortColumn: 0, sortAscending: false} );
 	}
 	
-	
-	
-	
+
 	function init_leads(){
 		//tabs
 		//we have to wait until the tabs have been created. Otherwise, the google chart table cannot get the correct width
@@ -104,9 +83,7 @@
 		}});
 	}
 
-
-
-
+	
 	function init_leads_table(leads) {
 		var divName='';
 		
@@ -150,42 +127,7 @@
 				leadType=$(this).attr("leadType");
 			showLocation(leads[leadType][idx]);
 		});
-		
-		
-		
-		//use google chart table
-//		var data = new google.visualization.DataTable();
-//		data.addColumn('number', 'Score');
-//		data.addColumn('string', 'User');
-//		data.addColumn('string', 'Tweet');
-//		//data.addColumn('string', 'Location');
-//		
-//		for (var indx in leadsGroup) {
-//			var lead = leadsGroup[indx];
-//			data.addRow( [ 
-//								lead.score,
-//								"<a style='color: #22A' title= 'Click to see twitter page.' target='_blank' href='http://www.twitter.com/" + lead.user + "'>" + lead.user + "</a>",
-//								pathgeo.util.highlightKeyword(app.constants.KEYWORDS, lead.text, true)
-//								//lead.loc
-//							] );
-//		}
-//		
-//		var table = new google.visualization.Table(document.getElementById(divName));
-//		table.draw(data, { showRowNumber: false, sortColumn: 0, sortAscending: false,  allowHtml: true});
-//				
-//		google.visualization.events.addListener(table, 'select', function() { 
-//			var row = table.getSelection()[0].row;
-//			
-//			//Can I still use the "data" variable due to closure??  Is this safe? (Chris)
-//			//YES, you can. the "data" varaible has become a global variable for this function. Therefore, even this function is a select-event listener, the "data" variable can be called correctly. (Calvin)
-//			var user = data.getFormattedValue(row, 1);
-//
-//			//Note: $(user).text() strips HTML tags, but not sure it is the best methods (Chris)
-//			showUserInfoDialog($(user).text());
-//		});
-
 	}
-	
 	
 	
 	function showUserInfoDialog(lead) {
@@ -210,19 +152,7 @@
 			}
 		});
 		
-		
-//		var userInfo;
-//		for (var indx in userData) {
-//			if (userData[indx].user_info.screen_name == userName) 
-//				userInfo = userData[indx].user_info;
-//		}
-		//alert(userInfo.image_url);
-//		$("#user_image").attr({"src": userInfo.image_url});
-//		$("#user_description").text(userInfo.description);
-//		$("#user_location").text(userInfo.location);
-//		$("#user_friends_count").text(userInfo.friends_count);
-//		$("#user_followers_count").text(userInfo.followers_count);
-		
+
 		showDialog('dialog_user_info', "About "+ lead.user, {
 			modal:true,
 			create:function(e,ui){
@@ -234,8 +164,6 @@
 		}); 
 		
 	}
-	
-	
 	
 	
 	/**
@@ -645,8 +573,6 @@
 			}
 		})
 		
-		
-		
 		//close all dialog
 		$("*").dialog("close");
 		
@@ -712,7 +638,6 @@
 	}
 	
 
-	
 	/**
 	 * sort array based on objname
 	 */
